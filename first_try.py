67ba33abe8b9270e626598481bbc04e1768981c8
ajotikaramrita1 <42275993+ajotikaramrita1@users.noreply.github.com>
2021-04-07 17:38:16 +0530

    Add files via upload

---
+++ first_try.py
@@ -0,0 +1,32 @@
+### integer
+a=10
+b=20
+c=b/a
+### String value
+'''
+name = "Rashmi"
+s_name = 'roma'
+s1_name = 'tejas'
+s2_name = """amrita"""
+'''
+
+### Float 
+print(c)
+print(a,b,c)
+#print(name, s_name, s1_name, s2_name)
+
+### user input
+name = input("enter your name")
+print(name)
+print("data type of name is",type(name))
+print("data type of a is",type(a))
+print("data type of c is",type(c))
+
+
+
+
+
+
+
+
+      
